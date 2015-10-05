9f6d4f02c6d7cf92806bc1478681bb2f5276da2b
root <root@AlexanderShpiller2.mylabserver.com>
2015-10-05 13:44:44 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["AlexanderShpiller3"] = { "site_title" => "Alex3s websites coming soon!", "port" => 80, "domain" => "AlexanderShpiller3.mylabserver.com" }
+default["apache"]["sites"]["AlexanderShpiller3b"] = { "site_title" => "Alex3b websites coming soon!", "port" => 80, "domain" => "AlexanderShpiller3b.mylabserver.com" }
+default["apache"]["sites"]["AlexanderShpiller4"] = { "site_title" => "Alex4s website!", "port" => 80, "domain" => "AlexanderShpiller4.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
