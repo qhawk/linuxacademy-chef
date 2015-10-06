f4af6b4f8eb5cf67ce0acb323969ca24ac094567
root <root@workstation.(none)>
2015-10-06 09:00:35 +0000

    Updating out workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["AlexanderShpiller3"] = { "site_title" => "Alex3s web
 default["apache"]["sites"]["AlexanderShpiller3b"] = { "site_title" => "Alex3b websites coming soon!", "port" => 80, "domain" => "AlexanderShpiller3b.mylabserver.com" }
 default["apache"]["sites"]["AlexanderShpiller4"] = { "site_title" => "Alex4s website!", "port" => 80, "domain" => "AlexanderShpiller4.mylabserver.com" }
 
+default["author"]["name"] = "Alex"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
